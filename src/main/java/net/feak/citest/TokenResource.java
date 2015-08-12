33099b486f4c56ed2fcd581ff81041e8d23c6de7
tfeak <tfeak@redemptiongames.com>
2015-08-12 14:07:27 -0700

    Break the build on purpose to check CI

--- src/main/java/net/feak/citest/TokenResource.java
+++ src/main/java/net/feak/citest/TokenResource.java
@@ -1,22 +1,14 @@
 package net.feak.citest;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import com.codahale.metrics.annotation.ExceptionMetered;
 import com.codahale.metrics.annotation.Metered;
 import com.codahale.metrics.annotation.Timed;
-import com.google.common.base.Strings;
 
 @Path("/token/{id}")
 @Produces(MediaType.APPLICATION_JSON)
@@ -27,6 +19,6 @@ public class TokenResource {
     @Metered(name = "getTokenRate")
     @ExceptionMetered(name = "getTokenExceptionRate")
     public Token getToken(@PathParam("id") String id) {
-        return new Token(id);
+        return new Token(id + "break it");
     }
 }
